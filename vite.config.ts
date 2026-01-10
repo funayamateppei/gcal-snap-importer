066e120f3362891fe3f59a32e44a8a287536c494
funayamateppei <t.funayama0217@gmail.com>
2026-01-11 01:16:39 +0900

    fix: deploy to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gcal-snap-importer/',
   server: {
     headers: {
       // Google OAuth ポップアップとの互換性のため
